72160a77d0f42d2f0fcfced5f9d3dcfea5037a2d
Leb14 <haocj98@gmail.com>
2024-09-19 21:44:25 +0800

    Setup backend environment
    - install express
    - install mongoose
    - connect to MongoDB

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGOURL = process.env.MONGO_URL;
+
+mongoose.connect(MONGOURL).then(()=>{
+  console.log("Success");
+  app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`)
+  });
+}).catch((error)=>console.log(error));
