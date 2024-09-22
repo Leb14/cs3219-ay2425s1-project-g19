fbdec06c217f16f9b6a7485c03719f0641a90ea2
Kevin <e1053635@u.nus.edu>
2024-09-23 01:28:53 +0800

    Add the "Add Question" function

--- frontend/src/pages/AddQuestion.js
+++ frontend/src/pages/AddQuestion.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddQuestion = () => {
+  const [questionName, setQuestionName] = useState("");
+  const [dateCreated, setDateCreated] = useState("");
+  const [category, setCategory] = useState("");
+  const [complexity, setComplexity] = useState("");
+  const [description, setDescription] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    if (name === "questionName") {
+      setQuestionName(value);
+    } else if (name === "dateCreated") {
+      setDateCreated(value);
+    } else if (name === "category") {
+      setCategory(value);
+    } else if (name === "complexity") {
+      setComplexity(value);
+    } else if (name === "description") {
+      setDescription(value);
+    }
+  };
+
   return (
     <div>
       <div className="d-flex justify-content-between flex-wrap flex-md-nowwrap align-items-center pt-3 pb-2 mb-3 border-bottom">
@@ -32,6 +57,89 @@ const AddQuestion = () => {
           </div>
         </div>
       </div>
+
+      <form action="/add" method="post">
+        <div className="row form-group mb-4">
+          <div className="col">
+            <label htmlFor="questionName">Question Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="questionName"
+              name="questionName"
+              value={questionName}
+              placeholder="Question Name"
+              onChange={handleSubmit}
+              required
+            />
+          </div>
+
+          <div className="col">
+            <label htmlFor="dateCreated">Date Created</label>
+            <input
+              type="text"
+              className="form-control"
+              id="dateCreated"
+              name="dateCreated"
+              value={dateCreated}
+              placeholder="Date Created"
+              onChange={handleSubmit}
+              required
+            />
+          </div>
+        </div>
+
+        <div className="row form-group mb-4">
+          <div className="col">
+            <label htmlFor="category">Category</label>
+            <input
+              type="text"
+              className="form-control"
+              id="category"
+              name="category"
+              value={category}
+              placeholder="Category"
+              onChange={handleSubmit}
+              required
+            />
+          </div>
+
+          <div className="col">
+            <label htmlFor="complexity">Complexity</label>
+            <input
+              type="text"
+              className="form-control"
+              id="complexity"
+              name="complexity"
+              value={complexity}
+              placeholder="Complexity"
+              onChange={handleSubmit}
+              required
+            />
+          </div>
+        </div>
+
+        <div className="form-group mb-4">
+          <label htmlFor="description">Description</label>
+          <textarea
+            className="form-control"
+            name="description"
+            id="description"
+            cols="30"
+            rows="12"
+            value={description}
+            placeholder="Description"
+            onChange={handleSubmit}
+            required
+          ></textarea>
+        </div>
+
+        <div className="form-group mb-4">
+          <button type="submit" className="btn btn-primary">
+            Add Question
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
