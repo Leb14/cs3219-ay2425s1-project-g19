e33484e0a3f2d1b9c83c206a0413687e18b6461f
Kevin <e1053635@u.nus.edu>
2024-09-22 22:00:53 +0800

    Add sidebar and header components to layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/main.css";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+
+      <div className="container-fluid">
+        <div className="row">
+          <Sidebar />
+          <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+            <h2>Dashboard</h2>
+          </main>
+        </div>
+      </div>
+    </>
   );
 }
 
