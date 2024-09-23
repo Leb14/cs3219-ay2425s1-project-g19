ef18a2fe2d8f666bab4597e06587126ed560c20b
Kevin <e1053635@u.nus.edu>
2024-09-23 16:09:28 +0800

    Add the list UI

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,6 @@ import React from "react";
 const Header = () => {
   return (
     <header className="header navbar navbar-dark flex-md-nowrap shadow">
-      
       <form
         className="nav col-12 col-md-auto flex-fill justify-content-center"
         role="search"
