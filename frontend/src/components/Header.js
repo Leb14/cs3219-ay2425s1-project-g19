a1278a234b86fe73ee51aea042421eb6ef638aac
jinnh14 <haocj98@gmail.com>
2024-09-23 10:22:37 +0800

    Change frontend design
    - follow the color scheme at Figma

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,26 +1,11 @@
 import React from "react";
 
-const header = () => {
+const Header = () => {
   return (
-    <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-      <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="/">
-        NodeJs
-      </a>
-
-      <button
-        class="navbar-toggler position-absolute d-md-none collapsed"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#sidebarMenu"
-        aria-controls="sidebarMenu"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
+    <header className="header navbar navbar-dark flex-md-nowrap shadow">
+      
       <form
-        class="nav col-12 col-md-auto flex-fill mb-2 justify-content-center mb-md-0"
+        className="nav col-12 col-md-auto flex-fill justify-content-center"
         role="search"
         method="POST"
         action="/search"
@@ -28,7 +13,7 @@ const header = () => {
         <input
           type="search"
           name="searchTerm"
-          class="form-control form-control-dark w-100 rounded-0 border-0"
+          className="form-control form-control-dark w-100 rounded-0 border-0"
           placeholder="Search..."
           aria-label="Search"
         />
@@ -37,4 +22,4 @@ const header = () => {
   );
 };
 
-export default header;
+export default Header;
---
+++ frontend/src/pages/Questions.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+
+const Questions = () => {
+  return (
+    <div>
+      {/* Keep the header here */}
+      <Header />
+
+      {/* Add content wrapper to push content down from under the header */}
+      <div className="content">
+        <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3">
+         <h1 className="h2 h2-styled">
+            Questions
+          </h1>
+          <div className="btn-toolbar mb-2 mb-md-0">
+            <div className="btn-group me-2">
+              <Link to="/add" className="btn btn-sm btn-outline-secondary">
+                + New Question
+              </Link>
+            </div>
+          </div>
+        </div>
+        <hr style={{ margin: "10px 15px", color: "white"}} />
+      </div>
+    </div>
+  );
+};
+
+export default Questions;
