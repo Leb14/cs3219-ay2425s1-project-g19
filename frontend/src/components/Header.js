c4a998ff2d536547c9fd42a38be3a210c48554bd
IamZhenHong <109903639+IamZhenHong@users.noreply.github.com>
2024-09-24 14:52:30 +0800

    Revert "Implement search function logic Beta"
    This reverts commit 48e7113bc3595267e5e93702739587af4258e144.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,24 +1,24 @@
-// import React from "react";
+import React from "react";
 
-// const Header = () => {
-//   return (
-//     <header className="header navbar navbar-dark flex-md-nowrap shadow">
-//       <form
-//         className="nav col-12 col-md-auto flex-fill justify-content-center"
-//         role="search"
-//         method="POST"
-//         action="/search"
-//       >
-//         <input
-//           type="search"
-//           name="searchTerm"
-//           className="form-control form-control-dark w-100 rounded-0 border-0"
-//           placeholder="Search..."
-//           aria-label="Search"
-//         />
-//       </form>
-//     </header>
-//   );
-// };
+const Header = () => {
+  return (
+    <header className="header navbar navbar-dark flex-md-nowrap shadow">
+      <form
+        className="nav col-12 col-md-auto flex-fill justify-content-center"
+        role="search"
+        method="POST"
+        action="/search"
+      >
+        <input
+          type="search"
+          name="searchTerm"
+          className="form-control form-control-dark w-100 rounded-0 border-0"
+          placeholder="Search..."
+          aria-label="Search"
+        />
+      </form>
+    </header>
+  );
+};
 
-// export default Header;
+export default Header;
