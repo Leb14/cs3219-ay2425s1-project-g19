3eca5beca0754ec163881ca9b290af967817850c
jinnh14 <haocj98@gmail.com>
2024-10-03 16:05:45 +0800

    Fix scroll bar glitch at auth pages

--- frontend/src/pages/user/Login.js
+++ frontend/src/pages/user/Login.js
@@ -1,13 +1,15 @@
 import React from "react";
 import LoginBox from "../../components/LoginBox";
+import useDisableScroll from "../../hooks/useDisableScroll"; 
 
 const Login = ({ onLogin }) => {
+  useDisableScroll();
   const handleLogin = (isAdmin) => {
     onLogin();
   };
 
   return (
-    <div>
+    <div className="auth">
       <div className="content">
         <LoginBox onLogin={handleLogin} />  {/* Pass onLogin as a prop */}
       </div>
--- frontend/src/pages/user/Register.js
+++ frontend/src/pages/user/Register.js
@@ -1,13 +1,15 @@
 import React from "react";
 import RegisterBox from "../../components/RegisterBox";
+import useDisableScroll from "../../hooks/useDisableScroll";
 
 const Register = ({ onRegister }) => {
+  useDisableScroll();
   const handleRegister = () => {
     onRegister();
   };
 
   return (
-    <div>
+    <div className="auth">
       <div className="content">
         <RegisterBox onRegister={handleRegister} />  {/* Pass onRegister as a prop */}
       </div>
